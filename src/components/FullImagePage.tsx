867a80b1301df1e22b755a6494d5e23ae1a84faa
theunhackable <srirangasaipagala@gmail.com>
2024-06-25 12:36:57 +0530

    addded metadata for the image

--- src/components/FullImagePage.tsx
+++ src/components/FullImagePage.tsx
@@ -1,18 +1,25 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId)
   return (
-    <div className="flex h-full w-full min-w-0 justify-between">
-      <div className="flex-shrink flex justify-center items-center">
+    <div className="flex h-full w-full min-w-0 justify-between border-t">
+      <div className="flex items-center justify-center p-5">
         <img
           src={image.url}
           alt={image.name}
-          className=" object-fit flex-shrink"
+          className=" object-contain"
         />
       </div>
-      <div className="flex w-48 flex-col border-l">
-        <div className="text-xl font-bold text-white">{image.name}</div>
+      <div className="flex w-48 flex-col border-l p-5 text-white flex-grow">
+          <h4 className="py-4 text-xl font-bold">{image.name}</h4>
+        <div className="flex flex-col gap-2 ">
+
+          {uploaderInfo.fullName && <p>Uploaded by: {uploaderInfo.fullName}</p>}
+          <p>Created on: {image.createdAt.toDateString()}</p>
+        </div>
       </div>
     </div>
   );
